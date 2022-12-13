f9f4407d8c94c5144ee67670de36f0b5207fa5ae
RileyCode <ripple.shen31@gmail.com>
2022-12-13 16:07:35 +0800

    get Restaurant and menu

--- onlineOrder/src/main/java/com/laioffer/onlineOrder/controller/MenuInfoController.java
+++ onlineOrder/src/main/java/com/laioffer/onlineOrder/controller/MenuInfoController.java
@@ -4,6 +4,8 @@ package com.laioffer.onlineOrder.controller;
 import com.laioffer.onlineOrder.entity.Cart;
 import com.laioffer.onlineOrder.entity.MenuItem;
 import com.laioffer.onlineOrder.entity.Restaurant;
+import com.laioffer.onlineOrder.service.MenuInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,15 +18,18 @@ import java.util.List;
 @Controller
 public class MenuInfoController {
 
+    @Autowired
+    private MenuInfoService menuInfoService;
+
     @RequestMapping(value="/restaurant/{restaurantId}/menu", method = RequestMethod.GET)
     @ResponseBody
     public List<MenuItem> getMenus(@PathVariable("restaurantId") int restaurantId) {
-        return new ArrayList<>();
+        return menuInfoService.getAllMenuItem(restaurantId);
     }
 
-    @RequestMapping(value="/restaurant", method = RequestMethod.GET)
+    @RequestMapping(value="/restaurants", method = RequestMethod.GET)
     @ResponseBody
     public List<Restaurant> getRestaurants() {
-        return new ArrayList<>();
+        return menuInfoService.getRestaurants();
     }
 }
--- onlineOrder/src/main/java/com/laioffer/onlineOrder/dao/MenuInfoDao.java
+++ onlineOrder/src/main/java/com/laioffer/onlineOrder/dao/MenuInfoDao.java
@@ -1,4 +1,52 @@
 package com.laioffer.onlineOrder.dao;
 
+
+import com.laioffer.onlineOrder.entity.MenuItem;
+import com.laioffer.onlineOrder.entity.Restaurant;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class MenuInfoDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public List<Restaurant> getRestaurants() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createCriteria(Restaurant.class)
+                    .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY)
+                    .list();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    public List<MenuItem> getAllMenuItem(int restaurantId) {
+        try (Session session = sessionFactory.openSession()) {
+            Restaurant restaurant = session.get(Restaurant.class, restaurantId);
+            if(restaurant != null) {
+                return restaurant.getMenuItemList();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    public MenuItem getMenuItem(int menuItemId) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(MenuItem.class, menuItemId);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
 }
